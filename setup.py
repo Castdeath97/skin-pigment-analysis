ab6591ed479a77692f67803c7f7bd4d8fa671654
Ammar Hasan <b5045438@ncl.ac.uk>
2018-12-31 11:30:05 +0300

    initial commit with cookie cutter file hierarchy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Development and Analysis of Machine Learning Models for the Diagnosis of Pigmented Skin Lesions ',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
